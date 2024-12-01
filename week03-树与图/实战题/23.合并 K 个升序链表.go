143cfa95d943439492d285c2025382b44bf2a9d7
erlang-boy <5436994+erlang-boy@user.noreply.gitee.com>
2024-12-01 22:34:50 +0800

    feat: 第三周

--- week03-树与图/实战题/23.合并 K 个升序链表.go
+++ week03-树与图/实战题/23.合并 K 个升序链表.go
@@ -19,6 +19,56 @@
  */
 func mergeKLists(lists []*ListNode) *ListNode {
 
+	h := &Heap{}
+	heap.Init(h)
+	var head *ListNode = &ListNode{}
+	var tail *ListNode = head
+
+	for _, n := range lists {
+		if n != nil {
+			heap.Push(h, n)
+		}
+	}
+
+	for h.Len() != 0 {
+		n := heap.Pop(h).(*ListNode)
+		tail.Next = n
+		tail = tail.Next
+
+		if n.Next != nil {
+			heap.Push(h, n.Next)
+		}
+
+	}
+
+	return head.Next
+
+}
+
+type Heap []*ListNode
+
+func (n Heap) Len() int {
+	return len(n)
+}
+
+func (n Heap) Less(i int, j int) bool {
+	return n[i].Val < n[j].Val
+}
+
+func (n Heap) Swap(i, j int) {
+	n[i], n[j] = n[j], n[i]
+}
+
+func (n *Heap) Push(x any) {
+	*n = append(*n, x.(*ListNode))
+}
+
+func (n *Heap) Pop() any {
+
+	old := *n
+	x := old[len(old)-1]
+	*n = old[:len(old)-1]
+	return x
 }
 
 // @lc code=end
