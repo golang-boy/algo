7104ac914043dafd17f11fcc7555fea0d6ca70bd
erlang-boy <5436994+erlang-boy@user.noreply.gitee.com>
2024-12-30 07:39:40 +0800

    feat: 第七周 股票

--- week07-动态规划/实战题/买卖股票系列问题/188.买卖股票的最佳时机 IV.go
+++ week07-动态规划/实战题/买卖股票系列问题/188.买卖股票的最佳时机 IV.go
@@ -12,6 +12,56 @@
 // @lc code=start
 func maxProfit(k int, prices []int) int {
 
+	prices = append([]int{0}, prices...)
+
+	f := make([][][]int, len(prices))
+
+	for i := 0; i < len(prices); i++ {
+		f[i] = make([][]int, 2)
+		for j := 0; j < 2; j++ {
+			f[i][j] = make([]int, k+1) // 加1是为确保从1开始, 简化程序
+			for h := 0; h <= k; h++ {
+				f[i][j][h] = math.MinInt
+			}
+		}
+	}
+	f[0][0][0] = 0
+
+	for i := 1; i <= len(prices); i++ {
+		for j := 0; j < 2; j++ {
+			for h := 0; h <= k; k++ {
+
+				// 买
+				if h >= 1 {
+					f[i][1][h] = max(f[i][1][h], f[i-1][0][h-1]-prices[i])
+				}
+				// 卖
+				//  持仓时+prices[i] = 等现在不持仓的收益
+				f[i][0][h] = max(f[i][0][h], f[i-1][1][h]+prices[i])
+				// 不做
+				// 前一天的与现在的一样
+				f[i][j][h] = max(f[i][j][h], f[i-1][j][h])
+
+			}
+		}
+	}
+
+	// 最多k笔交易，因此需要找最大的那个
+	ans := math.MinInt
+
+	for h := 0; h <= k; h++ {
+		if ans < f[len(prices)-1][0][h] {
+			ans = f[len(prices)-1][0][h]
+		}
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 // @lc code=end
