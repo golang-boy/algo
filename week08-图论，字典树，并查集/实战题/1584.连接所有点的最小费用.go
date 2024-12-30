9317ed3a7a5f645c996f8ff32ba9ee88fff6927c
liuhuidong <root@localhost.localdomain>
2024-12-30 18:16:44 +0800

    feat: 第八周题目

---
+++ week08-图论，字典树，并查集/实战题/1584.连接所有点的最小费用.go
@@ -0,0 +1,41 @@
+
+/*
+ * @lc app=leetcode.cn id=1584 lang=golang
+ * @lcpr version=20004
+ *
+ * [1584] 连接所有点的最小费用
+ */
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+func minCostConnectPoints(points [][]int) int {
+
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// [[0,0],[2,2],[3,10],[5,2],[7,0]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[3,12],[-2,5],[-4,1]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[0,0],[1,1],[1,0],[-1,1]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[-1000000,-1000000],[1000000,1000000]]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [[0,0]]\n
+// @lcpr case=end
+
+*/
+
