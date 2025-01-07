ada6a3d1a90b6cba6e9530da69a2fb5ca1aa6f00
erlang-boy <5436994+erlang-boy@user.noreply.gitee.com>
2025-01-08 07:30:33 +0800

    feat: 前缀和模板题

--- week09-前缀和，差分，双指针，字符串/实战题/前缀和差分/304.二维区域和检索 - 矩阵不可变.go
+++ week09-前缀和，差分，双指针，字符串/实战题/前缀和差分/304.二维区域和检索 - 矩阵不可变.go
@@ -10,15 +10,52 @@
 // @lcpr-template-end
 // @lc code=start
 type NumMatrix struct {
+	sum [][]int
 }
 
 func Constructor(matrix [][]int) NumMatrix {
+	n := len(matrix)
+	m := len(matrix[0])
+
+	sum := make([][]int, n+1)
+	for i := range sum {
+		sum[i] = make([]int, m+1)
+	}
+
+	// 计算前缀和
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + matrix[i-1][j-1]
+		}
+	}
+
+	return NumMatrix{
+		sum: sum,
+	}
 
 }
 
 func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
-
+	row1++
+	row2++
+	col1++
+	col2++
+	return this.sum[row2][col2] - this.sum[row2][col1-1] - this.sum[row1-1][col2] + this.sum[row1-1][col1-1]
 }
+
+/*
+总结：
+    二维前缀和思想：
+        s[i][j] 表示从（0,0）到（i,j)的和
+
+        s[i][j] = s[i-1][j]+s[i][j-1]-s[i-1][j-1] + a[i][j]
+
+        sum[p,q,i,j] 表示从（p,q)到（i,j)的区域和
+
+        sum[p,q,i,j] = s[i][j] - s[i][q-1]-s[p-1][j] + s[p-1][q-1]
+
+        sumRegion实践复杂度为O(1)
+*/
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
