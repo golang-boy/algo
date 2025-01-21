a97215647619a7ec1e2ce8c668f217d5a3c97723
erlang-boy <5436994+erlang-boy@user.noreply.gitee.com>
2025-01-22 07:40:27 +0800

    feat: 第九周作业

--- week09-前缀和，差分，双指针，字符串/作业题/560.和为 K 的子数组.go
+++ week09-前缀和，差分，双指针，字符串/作业题/560.和为 K 的子数组.go
@@ -11,13 +11,48 @@
 // @lc code=start
 func subarraySum(nums []int, k int) int {
 	/*
-	   计算每个位置的前缀和
+		输入整数数组nums和k,返回连续子数组元素和为k的子数组个数
+
+		求取 sum[l,r] = k
+
+		sum[l,r] = sum[r] - sums[l-1] == k
+
+		sum[l-1] = sum[r] - k
 	*/
 
-	for {
+	n := len(nums)
 
+	sum := make([]int, n+1)
+
+	sum[0] = 0
+	ans := 0
+
+	for i := 1; i < n+1; i++ {
+		sum[i] = sum[i-1] + nums[i-1]
 	}
 
+	// for i := 1; i <= n; i++ {
+	// 	s := sum[i] - k
+	// 	for j := 0; j < i; j++ {
+	// 		if s == sum[j] {
+	// 			ans++
+	// 		}
+	// 	}
+	// }
+
+	// count用于计数，记录每个位置和为某个值的数量
+	// 如果和的值比较小，可以使用索引表示，则使用count比较快速
+	// 此处也可以使用map存储和为某个值数量的计数, 一轮循环边计算边统计
+
+	count := make(map[int]int)
+	count[sum[0]]++
+
+	for i := 1; i <= n; i++ {
+		s := sum[i] - k
+		ans += count[s] // 在count查找计数
+		count[sum[i]]++
+	}
+	return ans
 }
 
 // @lc code=end
--- week09-前缀和，差分，双指针，字符串/作业题/686.重复叠加字符串匹配.go
+++ week09-前缀和，差分，双指针，字符串/作业题/686.重复叠加字符串匹配.go
@@ -11,6 +11,19 @@
 // @lc code=start
 func repeatedStringMatch(a string, b string) int {
 
+	/*
+		a叠加多次后，b为a的子串
+		输出叠加的次数
+
+		要使条件满足:
+		   1. 则a必为b的子串
+		   2. 找到整个a在b中的位置
+		   3. 往左往右找 abc,bcd,或者ab,cd,a,d
+		   4. b像个环形数组
+		   	   先定位a在b中的位置，如果找到,则计数
+			   然后在环形数组中查找是否还有其他a(或者尾部的和头部的拼起来是否为a),如果是, 则+2
+	*/
+
 }
 
 // @lc code=end
