dda76e4a507fbfc5fe7dc08c8c00e21202072d30
erlang-boy <5436994+erlang-boy@user.noreply.gitee.com>
2025-01-23 23:59:57 +0800

    feat: 正则匹配

--- week09-前缀和，差分，双指针，字符串/实战题/字符串/10.正则表达式匹配.go
+++ week09-前缀和，差分，双指针，字符串/实战题/字符串/10.正则表达式匹配.go
@@ -10,6 +10,25 @@
 // @lcpr-template-end
 // @lc code=start
 func isMatch(s string, p string) bool {
+	/*
+		输入s和p, s为字符串，p为模式串。.表示匹配任一字符，*匹配0个或多个前面的元素
+		输出能否匹配
+
+		f[i][j] 在s的前i个字符[0,i-1]是否可以匹配p中前j个字符[0,j-1]
+
+
+		if p[j-1] == '.' || s[i-1] == p[j-1] {
+			f[i][j] = f[i-1][j-1]
+		}
+
+		if p[j-1] == '*' {
+			f[i][j] = f[i][j - 2] ||
+		}
+
+
+
+
+	*/
 
 }
 
