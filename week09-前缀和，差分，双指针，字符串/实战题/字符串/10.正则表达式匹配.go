ffa2debc5c0fc139a68ab1318749d5ebb8e3a699
erlang-boy <5436994+erlang-boy@user.noreply.gitee.com>
2025-01-25 22:53:52 +0800

    feat: 完成正则匹配

--- week09-前缀和，差分，双指针，字符串/实战题/字符串/10.正则表达式匹配.go
+++ week09-前缀和，差分，双指针，字符串/实战题/字符串/10.正则表达式匹配.go
@@ -31,19 +31,47 @@ func isMatch(s string, p string) bool {
 
 	*/
 
+	n := len(s)
+	m := len(p)
+
 	f := make([][]bool, len(s)+1)
 	for i := range f {
 		f[i] = make([]bool, len(p)+1)
 	}
 
+	s = " " + s
+	p = " " + p
+
 	f[0][0] = true
 
-	for j := 1; j <= len(p)+1; j++ {
-		if p[j-1] == '*' {
-			f[0][j] = f[0][j-2]
+	for j := 2; j <= m; j += 2 {
+		if p[j] == '*' {
+			f[0][j] = true
+		} else {
+			break
 		}
 	}
 
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if p[j] >= 'a' && p[j] <= 'z' {
+				f[i][j] = f[i-1][j-1] && s[i] == p[j]
+			} else if p[j] == '.' {
+				f[i][j] = f[i-1][j-1]
+			} else {
+				// p[j] == '*' 情况, 可以匹配前面s[i-1]位置任何值，同时需要参考更前面的是否匹配，更前面的为f[i][j-2]
+				f[i][j] = f[i][j-2]
+
+				// p[j] 为*时，如果p[j-1] 为. (此时f[i][j] = f[i][j-2]) 或者为字母等于s[i]时，此时也匹配，同时要参考更前面i-1位置的
+				if s[i] == p[j-1] || p[j-1] == '.' {
+					f[i][j] = f[i][j] || f[i-1][j]
+				}
+			}
+
+		}
+	}
+
+	return f[n][m]
 }
 
 // @lc code=end
